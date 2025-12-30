e82da3dc27d59d50c2d9bd0400dc9254dce0ba2b
logongas <lorenzo.profesor@gmail.com>
2025-12-30 20:09:10 +0100

    flyway

--- src/main/java/com/educaflow/secretariavirtual/startup/Main.java
+++ src/main/java/com/educaflow/secretariavirtual/startup/Main.java
@@ -4,27 +4,13 @@ import com.axelor.app.AppSettings;
 import com.axelor.app.AvailableAppSettings;
 import com.axelor.app.AxelorModule;
 import com.educaflow.base.infrastructure.criptografia.EntornoCriptografico;
-import com.educaflow.base.infrastructure.criptografia.config.AlmacenCertificadosConfiablesConfig;
 import com.educaflow.base.infrastructure.criptografia.config.EntornoCriptograficoConfig;
-import com.educaflow.base.infrastructure.criptografia.config.DispositivoCriptograficoConfig;
-import com.educaflow.base.infrastructure.db.BulkTables;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import java.io.InputStream;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 public class Main extends AxelorModule {
 
+    @Override
     protected void configure() {
+        System.out.println("Iniciando Aplicación Secretaria Virtual...");
 
         configureEntornoCriptografico();
         configureDatabase();
@@ -40,19 +26,10 @@ public class Main extends AxelorModule {
     }
 
     private void configureDatabase() {
-        String dataBaseDriver = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_DRIVER);
-        String dataBaseURL = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_URL);
-        String dataBaseUser = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_USER);
-        String dataBasePassword = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_PASSWORD);
-        String schemaName = "public";
-
-
-
         TruncateTables truncateTables = new TruncateTables();
-        truncateTables.truncateTables(dataBaseDriver,dataBaseURL,dataBaseUser,dataBasePassword,schemaName);
+        truncateTables.truncateTables();
 
-        DataBaseMigrate dataBaseMigrate = new DataBaseMigrate();
-        dataBaseMigrate.migrate(dataBaseDriver,dataBaseURL,dataBaseUser,dataBasePassword,schemaName);
+        bind(DataBaseMigrate.class);
     }
 
 
--- src/main/java/com/educaflow/secretariavirtual/startup/TruncateTables.java
+++ src/main/java/com/educaflow/secretariavirtual/startup/TruncateTables.java
@@ -1,12 +1,22 @@
 package com.educaflow.secretariavirtual.startup;
 
+import com.axelor.app.AppSettings;
+import com.axelor.app.AvailableAppSettings;
 import com.educaflow.base.infrastructure.db.BulkTables;
 
 import java.util.Set;
 
 public class TruncateTables {
 
-    public void truncateTables(String dataBaseDriver, String dataBaseURL, String dataBaseUser, String dataBasePassword, String schemaName)  {
+    public void truncateTables()  {
+        String dataBaseDriver = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_DRIVER);
+        String dataBaseURL = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_URL);
+        String dataBaseUser = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_USER);
+        String dataBasePassword = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_PASSWORD);
+        String schemaName = "public";
+
+
+
         Set<String> tablasExcluidas = Set.of("meta_file", "meta_sequence", "auth_user", "auth_group", "meta_filter");
         Set<String> tablasIncluidas = Set.of("expedientes_estado_tipo_expediente");
 
