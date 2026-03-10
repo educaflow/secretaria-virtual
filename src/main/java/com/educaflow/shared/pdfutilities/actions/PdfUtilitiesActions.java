f234a524ad67f6aed3b0dcb564710d74e5174b46
logongas <lorenzo.profesor@gmail.com>
2026-03-10 22:06:00 +0100

    Hacer las marcas de la Y cada 50 en los campos de la firma

--- src/main/java/com/educaflow/shared/pdfutilities/actions/PdfUtilitiesActions.java
+++ src/main/java/com/educaflow/shared/pdfutilities/actions/PdfUtilitiesActions.java
@@ -46,8 +46,8 @@ public class PdfUtilitiesActions {
 
 
             for(int x=0;x<=500;x+=100) {
-                for(int y=0;y<=700;y+=100) {
-                    CampoFirma campoFirma=new CampoFirma(new Rectangulo(x,y,100,100)).setNumeroPagina(1).setMensaje(x+"," + y);
+                for(int y=0;y<=700;y+=50) {
+                    CampoFirma campoFirma=new CampoFirma(new Rectangulo(x,y,100,20)).setNumeroPagina(1).setMensaje(x+"," + y);
                     AlmacenClave almacenClave=almacenClaveLoader.getDummy();
                     documentoPdf=documentoPdf.firmar(almacenClave,campoFirma);
                 }
