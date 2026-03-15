21ecfddcb4243fee9b2a8c3fc807d6be0671775f
logongas <lorenzo.profesor@gmail.com>
2026-03-15 22:30:44 +0100

    Que no de el mensaje de error:"Failed to load cache configuration"
    Failed to load cache configuration com.typesafe.config.ConfigException$BadPath: path parameter: Invalid path 'caffeine.jcache.hibernate:.com.axelor.meta.db.MetaModule': Token not allowed in path expression: ':' (you can double-quote this token if you really want it here)

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ set -e
 clear
 #docker stop educaflow-db
 #docker run --name educaflow-db --hostname educaflow-db  -e POSTGRES_USER=educaflow -e POSTGRES_PASSWORD=educaflow -e POSTGRES_DB=educaflow -p 5432:5432 -d --rm postgres:12.22
+#./gradlew clean build --info --refresh-dependencies
 ./gradlew clean build --info
 # ./gradlew clean test --info
 #./gradlew --no-daemon run --debug-jvm --port 8080 --context-path /
