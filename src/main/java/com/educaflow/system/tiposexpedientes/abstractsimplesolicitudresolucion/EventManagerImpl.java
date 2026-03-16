8cfb650e1c36b7be0a532d0d4f4bcdc78b266dff
logongas <lorenzo.profesor@gmail.com>
2026-03-16 11:54:43 +0100

    Mover los servicios de los expedientes a subcarpetas

---
+++ src/main/java/com/educaflow/system/tiposexpedientes/abstractsimplesolicitudresolucion/EventManagerImpl.java
@@ -0,0 +1,90 @@
+package com.educaflow.system.tiposexpedientes.abstractsimplesolicitudresolucion;
+
+import com.axelor.inject.Beans;
+import com.educaflow.subsystem.expedientes.services.eventmanager.EventContext;
+import com.educaflow.subsystem.expedientes.services.eventmanager.OnEnterState;
+import com.educaflow.subsystem.expedientes.services.eventmanager.WhenEvent;
+import com.educaflow.subsystem.expedientes.db.AbstractSimpleSolicitudResolucion;
+import com.educaflow.subsystem.expedientes.db.repo.AbstractSimpleSolicitudResolucionRepository;
+import com.educaflow.base.infrastructure.validation.messages.BusinessException;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class EventManagerImpl extends com.educaflow.subsystem.expedientes.services.eventmanager.EventManager<AbstractSimpleSolicitudResolucion, AbstractSimpleSolicitudResolucion.State, AbstractSimpleSolicitudResolucion.Event,AbstractSimpleSolicitudResolucion.Profile> {
+
+    private final AbstractSimpleSolicitudResolucionRepository repository;
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Inject
+    public EventManagerImpl(AbstractSimpleSolicitudResolucionRepository repository) {
+        super(AbstractSimpleSolicitudResolucion.class, AbstractSimpleSolicitudResolucion.State.class, AbstractSimpleSolicitudResolucion.Event.class,AbstractSimpleSolicitudResolucion.Profile.class);
+        this.repository = repository;
+    }
+
+    @Override
+    public void triggerInitialEvent(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) throws BusinessException {
+
+
+    }
+
+
+    @WhenEvent
+    public void triggerDelete(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, AbstractSimpleSolicitudResolucion original, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) throws BusinessException {
+        //abstractSimpleSolicitudResolucion.updateState(AbstractSimpleSolicitudResolucion.State.);
+    }
+    @WhenEvent
+    public void triggerGuardarDatos(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, AbstractSimpleSolicitudResolucion original, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) throws BusinessException {
+        //abstractSimpleSolicitudResolucion.updateState(AbstractSimpleSolicitudResolucion.State.);
+    }
+    @WhenEvent
+    public void triggerBack(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, AbstractSimpleSolicitudResolucion original, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) throws BusinessException {
+        //abstractSimpleSolicitudResolucion.updateState(AbstractSimpleSolicitudResolucion.State.);
+    }
+    @WhenEvent
+    public void triggerPresentar(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, AbstractSimpleSolicitudResolucion original, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) throws BusinessException {
+        //abstractSimpleSolicitudResolucion.updateState(AbstractSimpleSolicitudResolucion.State.);
+    }
+    @WhenEvent
+    public void triggerResolver(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, AbstractSimpleSolicitudResolucion original, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) throws BusinessException {
+        //abstractSimpleSolicitudResolucion.updateState(AbstractSimpleSolicitudResolucion.State.);
+    }
+
+
+
+/***************************************************************************************/
+/*************************************** Estados ***************************************/
+/***************************************************************************************/
+
+    @OnEnterState
+    public void onEnterEntradaDatos(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) {
+
+    }
+    @OnEnterState
+    public void onEnterPendientePresentacion(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) {
+
+    }
+    @OnEnterState
+    public void onEnterPendienteResolucion(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) {
+
+    }
+    @OnEnterState
+    public void onEnterAceptado(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) {
+
+    }
+    @OnEnterState
+    public void onEnterRechazado(AbstractSimpleSolicitudResolucion abstractSimpleSolicitudResolucion, EventContext<AbstractSimpleSolicitudResolucion.Profile> eventContext) {
+
+    }
+
+
+
+
+
+
+
+
+
+}
