c8c9dfc192c3901efb35e8c4cc1f53eb704cc7be
logongas <lorenzo.profesor@gmail.com>
2026-04-07 19:47:47 +0200

    Nueva función de validación de firmas de PDF "FirmaPdf" para usar en las validaciones de los expedientes

---
+++ src/main/java/com/educaflow/base/infrastructure/validation/rules/PdfRules.kt
@@ -0,0 +1,42 @@
+package com.educaflow.base.infrastructure.validation.rules
+
+import com.axelor.meta.db.MetaFile
+import com.educaflow.base.infrastructure.metafile.MetaFileHelper
+import com.educaflow.base.infrastructure.pdf.DocumentoPdfUtil
+import com.educaflow.base.infrastructure.validation.engine.ValidationRule
+import com.educaflow.base.infrastructure.validation.messages.BusinessMessages
+import kotlin.reflect.KCallable
+
+data class FirmaPdf(val documentoOriginalField: KCallable<*>, val dniField: KCallable<*>) : ValidationRule {
+
+    override fun validate(value: Any?, bean: Any): BusinessMessages? {
+        if (value == null) {
+            return null
+        }
+
+        if (value !is MetaFile) {
+            return null
+        }
+
+        val metaFileOriginal = documentoOriginalField.call(bean)
+        if (metaFileOriginal !is MetaFile) {
+            return null
+        }
+
+        val dni = dniField.call(bean)
+        if (dni !is String) {
+            return null
+        }
+
+        val documentoOriginal = MetaFileHelper.getDocumentoPdf(metaFileOriginal)
+        val documentoFirmado = MetaFileHelper.getDocumentoPdf(value)
+
+        val errorMessage = DocumentoPdfUtil.validateFirmaPdf(documentoOriginal, documentoFirmado, dni)
+
+        if (errorMessage.isPresent) {
+            return BusinessMessages.single(errorMessage.get())
+        } else {
+            return null
+        }
+    }
+}
