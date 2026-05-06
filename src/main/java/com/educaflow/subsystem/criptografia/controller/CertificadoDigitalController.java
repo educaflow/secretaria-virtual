01fc95993c598d6a6b32f0ff8d51339da3c0cd19
logongas <lorenzo.profesor@gmail.com>
2026-05-06 17:43:39 +0200

    Nuevo subsistema de criptografia para permitir añadir certificados (incluidos los USB) en tiempo de ejecución

---
+++ src/main/java/com/educaflow/subsystem/criptografia/controller/CertificadoDigitalController.java
@@ -0,0 +1,45 @@
+package com.educaflow.subsystem.criptografia.controller;
+
+import com.axelor.db.modelservice.ModelServiceFactory;
+import com.axelor.meta.CallMethod;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.educaflow.base.infrastructure.axelorhelper.ActionRequestHelper;
+import com.educaflow.base.infrastructure.axelorhelper.ActionResponseHelper;
+import com.educaflow.base.infrastructure.validation.messages.BusinessMessages;
+import com.educaflow.base.util.AllowProperties;
+import com.educaflow.subsystem.criptografia.db.CertificadoDigital;
+import com.educaflow.subsystem.criptografia.service.CertificadoDigitalService;
+import com.google.inject.Inject;
+
+import java.util.Optional;
+
+public class CertificadoDigitalController {
+
+    @Inject
+    private ModelServiceFactory modelServiceFactory;
+
+    @CallMethod
+    public void validarCertificadoDigital(ActionRequest actionRequest, ActionResponse actionResponse) {
+        final CertificadoDigitalService service = (CertificadoDigitalService) modelServiceFactory.resolve(CertificadoDigital.class);
+
+        ActionRequestHelper<CertificadoDigital> actionRequestHelper = new ActionRequestHelper<>(actionRequest, CertificadoDigital.class);
+        ActionResponseHelper actionResponseHelper = new ActionResponseHelper(actionResponse);
+
+        AllowProperties allowProperties = AllowProperties.createAllowAllProperties();
+        CertificadoDigital certificado = actionRequestHelper.getModel(allowProperties);
+
+        Optional<BusinessMessages> validationResult;
+        if (actionRequestHelper.getId() == null) {
+            validationResult = service.validateInsert(certificado);
+        } else {
+            CertificadoDigital certificadoOriginal = actionRequestHelper.getOriginalModel();
+            validationResult = service.validateUpdate(certificado, certificadoOriginal);
+        }
+
+        if (validationResult.isPresent()) {
+            actionResponseHelper.doResponseBusinessMessagesAsError(validationResult.get());
+        }
+    }
+
+}
