e993ca118711aeaba9b1ef1e7839a32eb61a8909
logongas <lorenzo.profesor@gmail.com>
2026-05-21 08:50:40 +0200

    Quitar todo lo de los correos

--- src/main/java/com/educaflow/subsystem/correos/service/AdjuntoCorreoService.java
+++
@@ -1,7 +0,0 @@
-package com.educaflow.subsystem.correos.service;
-
-import com.axelor.db.modelservice.ModelService;
-import com.educaflow.subsystem.correos.db.AdjuntoCorreo;
-
-public interface AdjuntoCorreoService extends ModelService<AdjuntoCorreo> {
-}
