f04ab2a81464649c9405695dcc630a65efc09c5d
logongas <lorenzo.profesor@gmail.com>
2026-06-26 03:39:01 +0200

    Migrar de flyway a data-init de Axelor

--- src/main/java/com/educaflow/secretariavirtual/startup/database/V0010__modulos.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO sistemaeducativo_modulo (id, version, created_on, created_by, code, NAME) SELECT nextval('sistemaeducativo_modulo_seq'), 0, CURRENT_TIMESTAMP, 1, 'DWS', 'Desarrollo Web Servidor' ;
-INSERT INTO sistemaeducativo_modulo (id, version, created_on, created_by, code, NAME) SELECT nextval('sistemaeducativo_modulo_seq'), 0, CURRENT_TIMESTAMP, 1, 'DWC', 'Desarrollo Web Cliente' ;
-INSERT INTO sistemaeducativo_modulo (id, version, created_on, created_by, code, NAME) SELECT nextval('sistemaeducativo_modulo_seq'), 0, CURRENT_TIMESTAMP, 1, 'DAW', 'Despliegue de Aplicaciones Web' ;
-INSERT INTO sistemaeducativo_modulo (id, version, created_on, created_by, code, NAME) SELECT nextval('sistemaeducativo_modulo_seq'), 0, CURRENT_TIMESTAMP, 1, 'DIW', 'Diseño de Interfaces Web' ;
-INSERT INTO sistemaeducativo_modulo (id, version, created_on, created_by, code, NAME) SELECT nextval('sistemaeducativo_modulo_seq'), 0, CURRENT_TIMESTAMP, 1, 'OPTATIVA2DAW', 'Testing' ;
-INSERT INTO sistemaeducativo_modulo (id, version, created_on, created_by, code, NAME) SELECT nextval('sistemaeducativo_modulo_seq'), 0, CURRENT_TIMESTAMP, 1, 'IPE1', 'Itinerario Personal para la Empleabilidad I' ;
